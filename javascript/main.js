aa985057a7f8583be9f8dbc143dd83da2d07ea2f
zZissou <kyle.edward.russell@gmail.com>
2016-11-05 19:45:06 -0700

    Fixed the scoreboard counter for car 2 as well as the css for the 2nd scoreboard

--- javascript/main.js
+++ javascript/main.js
@@ -7,33 +7,33 @@ $(document).ready(function() {
   var carOneWinner = $('.score_one');
   var carTwoWinner = $('.score_two');
   var carOneScore = 0;
-  var carTwoscore = 0;
+  var carTwoScore = 0;
 
   $('button').click(function() {
     $('.car_one').css('left', '160px');
     $('.car_two').css('left', '160px');
   });
+
   $(document).keydown(function(e) {
 
     if (e.keyCode === 191) {
       $('.car_one').css('left', '+=5px');
       var car_one = $('.car_one').css('left');
-      if ((parseInt($car_one.css('left'), startLine) > finishLine)) {
+      if ((parseInt(car_one, startLine) > finishLine)) {
         alert("Car One Wins!");
         carOneScore += 1;
-        $('.score_one').html(carOneScore);
+        $(carOneWinner).html(carOneScore);
         }
     }
+
     if (e.keyCode === 90) {
       $('.car_two').css('left', '+=5px');
       var car_two = $('.car_two').css('left');
-      if ((parseInt($car_one.css('left'), startLine) > finishLine)) {
+      if ((parseInt($car_two.css('left'), startLine) > finishLine)) {
         alert("Car Two Wins!");
+        carTwoScore +=1;
+        $(carTwoWinner).html(carTwoScore);
       }
     }
   });
-  //
-  //
-  //
-  // while (positionOne || positionTwo < )
 });
